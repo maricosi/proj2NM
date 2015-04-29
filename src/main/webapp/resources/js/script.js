90872ffeb6496cce55cb650ef9f9bf5422e68223
maricosi <marisaisimoes@gmail.com>
2015-04-29 18:02:17 +0100

    template

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -2,40 +2,11 @@
  * 
  */
 
-function changeFormAction(){
-	var tipo = "";
-	if (document.getElementById("calctypeform:tipo:0").checked){
-		tipo = document.getElementById("calctypeform:tipo:0").value;
-	}else if (document.getElementById("calctypeform:tipo:1").checked){
-		tipo = document.getElementById("calctypeform:tipo:1").value;
+function redirect(){
+	if(document.forms['header']['header:tipo'][0].checked == true){
+
+		document.location.href = '../pages/basic.xhtml'
+	}else if(document.forms['header']['header:tipo'][1].checked == true){
+		document.location.href = '../pages/scientific.xhtml'
 	}
-	
-	
-	
-	document.getElementById("calctypeform").action =  tipo+".xhtml";
-	
 }
-
-
-function reactToChangedRadio(){
-    alert("I'm in!");
-    var myval;
-     for(i=0;i<3;i++){
-             if(document.forms['myFormId']['myFormId:myRadio'][i].checked == true ){
-                 myval = document.forms['myFormId']['myFormId:myRadio'].text/value;
-             }
-         }
-         alert( "val = " + myval );
- }
-
-
-
-function uncheckRadioButtons(radioButton) {
-	var selectedRadioButton;
-	   if (selectedRadioButton != null) {
-	      selectedRadioButton.checked = false;
-	   }
-
-	   selectedRadioButton = radioButton;
-	   selectedRadioButton.checked = true;
-	}
